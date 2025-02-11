8f58efa71b541a615db68b6849f6cdfff78557be
aaroh <aarohkandy@gmail.com>
2025-02-10 17:25:26 -0800

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       aaroh_oi4kwfu                                                #
+# 	Created:      2/10/2025, 4:25:36 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
